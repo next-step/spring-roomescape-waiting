39c15e80006e94dbaf88360e9cad2638bc1dded7
MZC01-PARKJUN5 <parkjun5@mz.co.kr>
2024-06-25 17:35:29 +0900

    refactor: JPA 적용으로 초기 데이터 적용 방식 변경
     - ddl-auto를 통해 테이블 생성
     - data.sql에 index 초기값 변경
     TODO: insert 쿼리가 아니라 Spring을 통해 값이 입력되도록 변경 필요

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,24 @@
-INSERT INTO reservation_time (start_at)
-VALUES  ('10:00'),
-        ('12:00'),
-        ('14:00'),
-        ('16:00'),
-        ('18:00'),
-        ('20:00');
+INSERT INTO reservation_time (reservation_time_id, start_at)
+VALUES  (90001, '10:00'),
+        (90002, '12:00'),
+        (90003, '14:00'),
+        (90004, '16:00'),
+        (90005, '18:00'),
+        (90006, '20:00');
 
-INSERT INTO theme (name, description, thumbnail)
-VALUES ('고양이 테마', '고양이 카페에서 탈출하는 테마입니다', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('쏘우 테마', '심약자 주의! 공포 테마입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
-       ('포켓몬 테마', '포켓몬고를 설치하셔야 합니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
+INSERT INTO theme (theme_id, name, description, thumbnail)
+VALUES (90001, '고양이 테마', '고양이 카페에서 탈출하는 테마입니다', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       (90002, '쏘우 테마', '심약자 주의! 공포 테마입니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       (90003, '포켓몬 테마', '포켓몬고를 설치하셔야 합니다.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
-INSERT INTO member (name, email, password)
-VALUES  ('관리자', 'tester@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+INSERT INTO member (member_id, name, email, password)
+VALUES  (90001, '관리자', 'tester@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
-INSERT INTO member_role (name, member_id)
-VALUES  ('ADMIN', 1);
+INSERT INTO member_role (member_role_id, name, member_id)
+VALUES  (90001, 'ADMIN', 90001);
 
-INSERT INTO member (name, email, password)
-VALUES  ('신규_유저', 'newbie@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+INSERT INTO member (member_id, name, email, password)
+VALUES  (90002, '신규_유저', 'newbie@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
-INSERT INTO member_role (name, member_id)
-VALUES  ('GUEST', 2);
+INSERT INTO member_role (member_role_id, name, member_id)
+VALUES  (90002, 'GUEST', 90002);
--- src/test/resources/data_test.sql
+++ src/test/resources/data_test.sql
@@ -1,6 +1,5 @@
--- This is an empty data file to prevent data initialization
-INSERT INTO member (name, email, password)
-VALUES  ('테스트_마스터', 'master@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+INSERT INTO member (member_id, name, email, password)
+VALUES  (90001, '테스트_마스터', 'master@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
-INSERT INTO member_role (name, member_id)
-VALUES  ('ADMIN', 1);
+INSERT INTO member_role (member_role_id, name, member_id)
+VALUES  (90001, 'ADMIN', 90001);
