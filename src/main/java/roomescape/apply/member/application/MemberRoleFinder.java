e39dd492614cb675f608b89c80e2491e00445a71
MZC01-PARKJUN5 <parkjun5@mz.co.kr>
2024-06-25 17:47:49 +0900

    polishing

--- src/main/java/roomescape/apply/member/application/MemberRoleFinder.java
+++ src/main/java/roomescape/apply/member/application/MemberRoleFinder.java
@@ -3,7 +3,7 @@ package roomescape.apply.member.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.apply.member.domain.MemberRoleName;
-import roomescape.apply.member.domain.repository.MemberRoleRepository;
+import roomescape.apply.member.domain.MemberRoleRepository;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -19,9 +19,7 @@ public class MemberRoleFinder {
     }
 
     public Set<MemberRoleName> findRolesInMember(long memberId) {
-        return memberRoleRepository.findNamesByMemberId(memberId)
-                .stream()
-                .map(MemberRoleName::findRoleByName)
+        return memberRoleRepository.findNamesByMemberId(memberId).stream()
                 .collect(Collectors.toSet());
     }
 
