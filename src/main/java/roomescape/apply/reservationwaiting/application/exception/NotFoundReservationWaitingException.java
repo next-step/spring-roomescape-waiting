40e4d641579e554a2280476d5966809986cb2bee
MZC01-PARKJUN5 <parkjun5@mz.co.kr>
2024-07-11 17:58:31 +0900

    test: InMemoryReservationWaitingRepository implement logics

---
+++ src/main/java/roomescape/apply/reservationwaiting/application/exception/NotFoundReservationWaitingException.java
@@ -0,0 +1,7 @@
+package roomescape.apply.reservationwaiting.application.exception;
+
+import java.util.NoSuchElementException;
+
+public class NotFoundReservationWaitingException extends NoSuchElementException {
+
+}
--- src/test/java/roomescape/apply/reservationwaiting/infra/InMemoryReservationWaitingRepository.java
+++ src/test/java/roomescape/apply/reservationwaiting/infra/InMemoryReservationWaitingRepository.java
@@ -1,12 +1,13 @@
 package roomescape.apply.reservationwaiting.infra;
 
+import roomescape.apply.reservationtime.ui.dto.ReservationTimeResponse;
 import roomescape.apply.reservationwaiting.domain.ReservationWaiting;
 import roomescape.apply.reservationwaiting.domain.ReservationWaitingRepository;
+import roomescape.apply.reservationwaiting.ui.dto.ReservationWaitingResponse;
+import roomescape.apply.theme.ui.dto.ThemeResponse;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class InMemoryReservationWaitingRepository implements ReservationWaitingRepository {
@@ -39,10 +40,70 @@ public class InMemoryReservationWaitingRepository implements ReservationWaitingR
                                                                      LocalDateTime waitingTime
     ) {
         return map.values().stream()
-                           .filter(it -> it.getTheme().getId() == themeId)
-                           .filter(it -> it.getReservationDate().equals(date))
-                           .filter(it -> it.getTime().getId() == timeId)
-                           .filter(it -> it.getWaitingTime().isBefore(waitingTime))
-                           .count();
+                .filter(it -> it.getTheme().getId() == themeId)
+                .filter(it -> it.getReservationDate().equals(date))
+                .filter(it -> it.getTime().getId() == timeId)
+                .filter(it -> it.getWaitingTime().isBefore(waitingTime))
+                .count();
+    }
+
+    @Override
+    public List<ReservationWaitingResponse> findAllWithPositionByMemberId(Long memberId) {
+        List<ReservationWaiting> memberReservation = map.values().stream()
+                .filter(it -> it.getMemberId().equals(memberId))
+                .toList();
+
+        Map<Long, Integer> waitingPositionMap = new HashMap<>();
+
+        for (ReservationWaiting selected : memberReservation) {
+            int position = calculatePosition(selected);
+            waitingPositionMap.put(selected.getId(), position);
+        }
+
+
+        return memberReservation.stream()
+                .map(reservation -> {
+                    int waitingPosition = waitingPositionMap.get(reservation.getId());
+                    ThemeResponse themeResponse = ThemeResponse.from(reservation.getTheme());
+                    ReservationTimeResponse timeResponse = ReservationTimeResponse.from(reservation.getTime());
+                    return ReservationWaitingResponse.from(reservation, waitingPosition, themeResponse, timeResponse);
+                })
+                .toList();
+    }
+
+    @Override
+    public Optional<Long> findIdById(long id) {
+        if (map.containsKey(id)) {
+            return Optional.of(id);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteById(long id) {
+        map.remove(id);
+    }
+
+    @Override
+    public Optional<ReservationWaiting> findOldestWaitingByThemeIdAndDateAndTimeId(long themeId, String date, long timeId) {
+        return map.values().stream()
+                .filter(it -> it.getTheme().getId() == themeId)
+                .filter(it -> it.getReservationDate().equals(date))
+                .filter(it -> it.getTime().getId() == timeId)
+                .findFirst();
+    }
+
+    private int calculatePosition(ReservationWaiting selected) {
+        int position = 0;
+        for (ReservationWaiting each : map.values()) {
+            boolean isRoomEscape = selected.getTheme().getId().equals(each.getTheme().getId())
+                                   && selected.getReservationDate().equals(each.getReservationDate())
+                                   && selected.getTime().getId().equals(each.getTime().getId());
+            if (isRoomEscape && each.getWaitingTime().isBefore(selected.getWaitingTime())) {
+                position++;
+            }
+        }
+        return position;
     }
 }
