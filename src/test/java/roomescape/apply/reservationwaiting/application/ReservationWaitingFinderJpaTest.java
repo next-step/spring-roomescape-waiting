5ed78b67bbb5787a7898a576fe6b815e23f963a6
MZC01-PARKJUN5 <parkjun5@mz.co.kr>
2024-07-12 09:23:20 +0900

    test: Add ReservationWaitingFinder DataJpaTest

---
+++ src/test/java/roomescape/apply/reservationwaiting/application/ReservationWaitingFinderJpaTest.java
@@ -0,0 +1,133 @@
+package roomescape.apply.reservationwaiting.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import roomescape.apply.member.domain.Member;
+import roomescape.apply.member.domain.MemberRepository;
+import roomescape.apply.reservationtime.domain.ReservationTime;
+import roomescape.apply.reservationtime.domain.ReservationTimeRepository;
+import roomescape.apply.reservationwaiting.domain.ReservationWaitingRepository;
+import roomescape.apply.theme.domain.Theme;
+import roomescape.apply.theme.domain.ThemeRepository;
+import roomescape.support.config.QueryDslConfig;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static roomescape.support.MemberFixture.member;
+import static roomescape.support.MemberFixture.memberRequest;
+import static roomescape.support.ReservationWaitingFixture.reservationWaiting;
+import static roomescape.support.ReservationsFixture.reservationTime;
+import static roomescape.support.ReservationsFixture.theme;
+
+@DataJpaTest
+@Import(QueryDslConfig.class)
+@DisplayName("예약 대기 JPA 기반 테스트")
+class ReservationWaitingFinderJpaTest {
+
+    private ReservationWaitingFinder reservationWaitingFinder;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private ThemeRepository themeRepository;
+    @Autowired
+    private ReservationTimeRepository reservationTimeRepository;
+    @Autowired
+    private ReservationWaitingRepository reservationWaitingRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        var waitingPositionCalculator = new WaitingPositionCalculator(reservationWaitingRepository);
+        reservationWaitingFinder = new ReservationWaitingFinder(waitingPositionCalculator, reservationWaitingRepository);
+    }
+
+    @Test
+    @DisplayName("사용자의 예약 대기들을 쿼리가 많이 나가며 가져올 수 있다.")
+    void findReservationWaitingListByMemberId() {
+        // given
+        Theme theme = themeRepository.save(theme());
+        Member member1 = memberRepository.save(member());
+        Member member2 = memberRepository.save(member(memberRequest("member2@gmail.com")));
+        int idx = 0;
+        for (String time : List.of("09:00", "10:00", "11:00", "12:00", "13:00", "14:00")) {
+            idx++;
+            ReservationTime reservationTime = reservationTimeRepository.save(reservationTime(time));
+            if (idx > 4) {
+                reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, member1.getId()));
+                reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, member2.getId()));
+            } else {
+                reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, member2.getId()));
+            }
+        }
+        // when
+        var memberOneWaiting = reservationWaitingFinder.findReservationWaitingListByMemberId(member1.getId());
+        var memberTwoWaiting = reservationWaitingFinder.findReservationWaitingListByMemberId(member2.getId());
+
+        // then
+        assertThat(memberOneWaiting).hasSize(2);
+        assertThat(memberTwoWaiting).hasSize(6);
+    }
+
+
+    @Test
+    @DisplayName("사용자의 예약 대기들을 하나의 쿼리로 가져올 수 있다.")
+    void findReservationWaitingListByMemberIdOneQuery() {
+        // given
+        Theme theme = themeRepository.save(theme());
+        Member member1 = memberRepository.save(member());
+        Member member2 = memberRepository.save(member(memberRequest("member2@gmail.com")));
+        int idx = 0;
+        for (String time : List.of("09:00", "10:00", "11:00", "12:00", "13:00", "14:00")) {
+            idx++;
+            ReservationTime reservationTime = reservationTimeRepository.save(reservationTime(time));
+            if (idx > 4) {
+                reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, member1.getId()));
+                reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, member2.getId()));
+            } else {
+                reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, member2.getId()));
+            }
+        }
+        // when
+        var memberOneWaiting = reservationWaitingFinder.findReservationWaitingListByMemberIdOneQuery(member1.getId());
+        var memberTwoWaiting = reservationWaitingFinder.findReservationWaitingListByMemberIdOneQuery(member2.getId());
+
+        // then
+        assertThat(memberOneWaiting).hasSize(2);
+        assertThat(memberTwoWaiting).hasSize(6);
+    }
+
+    @Test
+    @DisplayName("가장 오래된 예약 대기를 가져올 수 있다.")
+    void findOldestReservationWaitingBy() {
+        // given
+        Theme theme = themeRepository.save(theme());
+        ReservationTime reservationTime = reservationTimeRepository.save(reservationTime());
+
+        Member member1 = memberRepository.save(member());
+        Member member2 = memberRepository.save(member(memberRequest("member2@gmail.com")));
+        Member member3 = memberRepository.save(member(memberRequest("member3@gmail.com")));
+
+        reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, "2099-12-02", member2.getId()));
+        reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, "2099-12-02", member1.getId()));
+        reservationWaitingRepository.save(reservationWaiting(reservationTime, theme, "2099-12-02", member3.getId()));
+        // when
+        var oldestReservationWaiting = reservationWaitingFinder.findOldestReservationWaitingBy(theme.getId(),
+                "2099-12-02",
+                reservationTime.getId());
+        var notExistReservationWaiting = reservationWaitingFinder.findOldestReservationWaitingBy(theme.getId(),
+                "2002-12-31",
+                reservationTime.getId());
+
+        // then
+        assertThat(oldestReservationWaiting).isPresent();
+        assertThat(oldestReservationWaiting.get().getMemberId()).isEqualTo(member2.getId());
+        assertThat(notExistReservationWaiting).isNotPresent();
+    }
+
+
+}
